4e0d404cd45c9e5ef6137b662e226080355f4ddc
lettell <lettell@DESKTOP-BLB8T63.home>
2019-05-05 18:17:13 +0300

    deploay

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,6 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 
-set :linked_files, %w{config/master.key config/database.yml}
 
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
@@ -36,8 +35,9 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/master.key config/database.yml}
+
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
