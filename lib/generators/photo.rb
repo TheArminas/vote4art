d6d8ed2ab17985984ca3684726840ce3485c9bd2
lettell <lettell@DESKTOP-BLB8T63.home>
2019-05-18 01:25:39 +0300

    quic fix

--- lib/generators/photo.rb
+++ lib/generators/photo.rb
@@ -1,6 +1,6 @@
 module Generators
   class Photo
-    END_POINT = 'https://apinode.vote4art.eu'
+    END_POINT = 'https://apinode.vote4art.eu/pixels'
 
     def initialize(pixels:)
       @pixels = pixels
@@ -12,12 +12,13 @@ module Generators
         'kasyra': 'naujas prikolas'
       }
   
-      res = HTTP.headers(headers).post(END_POINT,
-                                         json: {
-                                           pixels: pixels.to_json,
-                                           url: Class.const_get('Photo').last&.url
-                                         }
-                                       )
+      res = HTTP.headers(headers).post(
+        END_POINT,
+        json: {
+          pixels: pixels,
+          url: Class.const_get('Photo').last&.url ||= false
+        })
+        binding.pry
       if res.status.to_sym == :ok
         data = JSON.parse(res.body)
         ::Photo.create(url: data["photo_path"], name: data["domain"])
