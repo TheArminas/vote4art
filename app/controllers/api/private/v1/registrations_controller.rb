350bf6b6d52837f52f8debaf87193c7cba523bb7
lettell <lettell@localhost.localdomain>
2019-05-24 21:10:20 +0300

    ok

--- app/controllers/api/private/v1/registrations_controller.rb
+++ app/controllers/api/private/v1/registrations_controller.rb
@@ -9,7 +9,7 @@ module Api
           s = 'break'
           
           exist = User.find_by(uniid: permitted_params['uniid'])
-          return render json: { error: "Limitas pasiektas" }, status: 401  if exist.persisted?
+          render json: { error: "Limitas pasiektas" }, status: 401  if exist.present?
           user = User.create(permitted_params)  if exist.blank?
 
           
